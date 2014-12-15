288e5745b7913c0d06d24d7763f1ec74641d3408
aguelo <axel.jonsson@live.com>
2014-12-15 11:18:56 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Intro Test Screen</title>
+</head>
+<body>
+  <h2>Welcome!</h2>
+  <p>Some text...</p>
+</body>
+<footer>
+  <p>Powered by Aguel</p>
+</footer>
