c0788fcd538eea9f73f1c0b5ca62e2b49056b53f
aguelo <axel.jonsson@live.com>
2014-12-16 12:31:58 +0100

    Added div-tags for container and content

--- index.php
+++ index.php
@@ -5,39 +5,43 @@
   <title>Intro Test Screen</title>
 </head>
 <body>
-  <h2>Heading!</h2>
-  <p>
-    <?php
+   <div class="content">
+      <div class="container">
+         <h2>Heading!</h2>
+            <p>
+               <?php
 
-    $sql = "SELECT * FROM PATIENT ";
+               $sql = "SELECT * FROM PATIENT ";
 
-	if($mysqli = connect_db()) {
+	            if($mysqli = connect_db()) {
 
-		$result = $mysqli->query($sql);
-		print_r($mysqli->error);
-	}
+                  $result = $mysqli->query($sql);
+		            print_r($mysqli->error);
+	            }
 
-    echo '<table cellpadding="6" border="1">';
-    echo "<tr>";
-    echo "<th>Personnummer</th>";
-    echo "<th>Förnamn</th>";
-    echo "<th>Efternamn</th>";
-    echo "<th>Epostadress</th>";
-    echo "</tr>";
+               echo '<table cellpadding="6" border="1">';
+               echo "<tr>";
+               echo "<th>Personnummer</th>";
+               echo "<th>Förnamn</th>";
+               echo "<th>Efternamn</th>";
+               echo "<th>Epostadress</th>";
+               echo "</tr>";
 
-    while($myRow = $result->fetch_array()){
-    	echo "<tr>";
-      echo "<td>" . $myRow['p_number'] . "</td>";
-      echo "<td>" . $myRow['p_firstname'] . "</td>";
-      echo "<td>" . $myRow['p_lastname'] . "</td>";
-      echo "<td>" . $myRow['p_email'] . "</td>";
-      echo "</tr>";
-    }
-    echo "</table>";
+               while($myRow = $result->fetch_array()){
+    	             echo "<tr>";
+                   echo "<td>" . $myRow['p_number'] . "</td>";
+                   echo "<td>" . $myRow['p_firstname'] . "</td>";
+                   echo "<td>" . $myRow['p_lastname'] . "</td>";
+                   echo "<td>" . $myRow['p_email'] . "</td>";
+                   echo "</tr>";
+               }
+               echo "</table>";
 
-?>
-  </p>
-  <p>Here be dragons</p>
+               ?>
+            </p>
+            <p>Here be dragons</p>
+         </div>
+      </div>
 </body>
 <footer>
   <p>Powered by Aguel and friends</p>
