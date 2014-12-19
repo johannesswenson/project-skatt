2318bdb218f8020ba6f301588c56ed3ede7e7790
Magnus Ulenius <magnus.ulenius@gmail.com>
2014-12-19 12:17:32 +0100

    Formatering

--- login.php
+++ login.php
@@ -1,11 +1,16 @@
+<?php
+    include('db_connection.php');
+    require('functions.php');
+?>
 <!DOCTYPE html PUBLIC "-//w3c//DTD XHTMLm 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmnlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Webbskattningsportalen</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Webbskattningsportalen</title>
+    </head>
     <body>
         <div id="main">
             <h1>Skattning Login Session</h1>
